3aea9d4be0d9510da12e8ea0a1c97e999cc52113
vector_grizzardo <gbarboza@vectoritcgroup.com>
2020-08-08 14:52:28 -0300

    first commit front

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,34 @@
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Criado!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
---
+++ frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,72 @@
+import { map, catchError } from 'rxjs/operators';
+import { Product } from './product.model';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable, EMPTY } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl = "http://localhost:3001/products"
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string, isErro: Boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: isErro ? ['msg-erro'] : ['msg-sucesso']
+    })
+  }
+
+  handlesErro(e: any): Observable<any> {
+    console.log(e)
+    this.showMessage('Ocorreu um erro!', true)
+    return EMPTY
+  }
+
+  create(produto: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, produto).pipe(
+      map((obj) => obj),
+      catchError(e => this.handlesErro(e))
+    );
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError(e => this.handlesErro(e))
+    );
+  }
+
+  readById(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url).pipe(
+      map((obj) => obj),
+      catchError(e => this.handlesErro(e))
+    );
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product).pipe(
+      map((obj) => obj),
+      catchError(e => this.handlesErro(e))
+    );
+  }
+
+  delete(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj),
+      catchError(e => this.handlesErro(e))
+    );
+  }
+
+}
+
+
